7b5b84b6804b96d83207dbfd420da01073fef857
Roberto Javier Homs Puron <rhoms@cells.es>
2021-07-05 13:57:41 +0200

    Add console script

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup, find_packages
 
 setup(name="ni660x",
     version="0.1",
-    description="Paquete de ni",
+    description="RPC server for NI660X counter application",
     author="Alba sincotron",
-    install_requires=["nidaqmx","click","pyyaml",],
+    install_requires=["nidaqmx", "click", "pyyaml",],
+    entry_points={
+      'console_scripts': [
+          'ni660x_rpc=ni660x.application:main',
+      ]
+    },
     packages=find_packages(),
     test_suite="test",
     classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)",],

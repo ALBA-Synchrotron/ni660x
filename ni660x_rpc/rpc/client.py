0ce269609caca452f5469a757f548375e3ff148a
Roberto Javier Homs Puron <rhoms@cells.es>
2021-07-07 16:19:09 +0200

    Add logging

--- ni660x_rpc/rpc/client.py
+++ ni660x_rpc/rpc/client.py
@@ -1,11 +1,12 @@
 from xmlrpc.client import ServerProxy
 from threading import Lock
 from functools import lru_cache
-
+import logging
 
 @lru_cache(maxsize=8)
 def get_ni_client(addr):
     return NI660XRPCClient(addr)
+
 
 
 class NI660XRPCClient:
@@ -15,8 +16,12 @@ class NI660XRPCClient:
 
     def __getattr__(self, item):
         def func(*args, **kwargs):
+            logging.info('NI client enter %s', item)
             with self._lock:
-                return getattr(self._proxy, item)(*args, **kwargs)
+                result = getattr(self._proxy, item)(*args, **kwargs)
+                logging.info('NI client return %s', item)
+                return result
+
         func.__name__ = item
         setattr(self, item, func)
         return func

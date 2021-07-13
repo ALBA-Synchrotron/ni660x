623348fe0c34d0ab7c88f59dac3c70d4d984d6fd
Roberto Javier Homs Puron <rhoms@cells.es>
2021-07-13 16:48:37 +0200

    Exclude test module

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(name="ni660x",
     version="version='0.3.0'",
     description="RPC server for NI660X counter application",
     author="Alba sincotron",
-    install_requires=["nidaqmx", "click", "pyyaml",],
+    install_requires=["nidaqmx", "click", "pyyaml"],
     entry_points={
       'console_scripts': [
           'ni660x-rpc-server=ni660x.rpc.server:main',
       ]
     },
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     test_suite="test",
     classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)",],
     license="GNU General Public License v3"
